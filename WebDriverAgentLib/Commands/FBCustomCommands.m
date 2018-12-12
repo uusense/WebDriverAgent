6ebacf994e09b6fe9517b13a087e823f2ce84bd0
liuxiaodong <csusheep@hotmail.com>
2018-12-12 14:11:39 +0800

    增加获取运营商接口 /wda/netBrand； 增加获取网络类型接口 /wda/netType；

--- WebDriverAgentLib/Commands/FBCustomCommands.m
+++ WebDriverAgentLib/Commands/FBCustomCommands.m
@@ -10,7 +10,10 @@
 #import "FBCustomCommands.h"
 
 #import <XCTest/XCUIDevice.h>
+#import <CoreTelephony/CTCarrier.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
 
+#import "Reachability.h"
 #import "FBApplication.h"
 #import "FBConfiguration.h"
 #import "FBExceptionHandler.h"
@@ -53,6 +56,8 @@
     [[FBRoute GET:@"/wda/batteryInfo"] respondWithTarget:self action:@selector(handleGetBatteryInfo:)],
     [[FBRoute POST:@"/wda/pressButton"] respondWithTarget:self action:@selector(handlePressButtonCommand:)],
     [[FBRoute POST:@"/wda/siri/activate"] respondWithTarget:self action:@selector(handleActivateSiri:)],
+    [[FBRoute GET:@"/wda/netType"].withoutSession respondWithTarget:self action:@selector(handleGetNetType:)],
+    [[FBRoute GET:@"/wda/netBrand"].withoutSession respondWithTarget:self action:@selector(handleGetNetBrand:)],
   ];
 }
 
@@ -209,6 +214,70 @@
     return FBResponseWithError(error);
   }
   return FBResponseWithOK();
+}
+
++ (id<FBResponsePayload>)handleGetNetType:(FBRouteRequest *)request
+{
+  NSString *netconnType = @"";
+  Reachability *reach = [Reachability reachabilityWithHostName:@"www.apple.com"];
+  
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
+  switch ([reach currentReachabilityStatus]) {
+    case NotReachable: {
+      netconnType = @"no network";
+    }
+      break;
+    case ReachableViaWiFi: {
+      netconnType = @"Wifi";
+    }
+      break;
+    case ReachableViaWWAN: {
+      CTTelephonyNetworkInfo *info = [[CTTelephonyNetworkInfo alloc] init];
+      NSString *currentStatus = info.currentRadioAccessTechnology;
+      if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyGPRS"]) {
+        netconnType = @"GPRS";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyEdge"]) {
+        netconnType = @"2.75G EDGE";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyWCDMA"]){
+        netconnType = @"3G";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyHSDPA"]){
+        netconnType = @"3.5G HSDPA";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyHSUPA"]){
+        netconnType = @"3.5G HSUPA";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyCDMA1x"]){
+        netconnType = @"2G";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyCDMAEVDORev0"]){
+        netconnType = @"3G";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyCDMAEVDORevA"]){
+        netconnType = @"3G";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyCDMAEVDORevB"]){
+        netconnType = @"3G";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyeHRPD"]){
+        netconnType = @"HRPD";
+      }else if ([currentStatus isEqualToString:@"CTRadioAccessTechnologyLTE"]){
+        netconnType = @"4G";
+      }
+    }
+      break;
+    default:
+      break;
+  }
+  NSLog(@"netconnType is %@", netconnType);
+#pragma clang diagnostic pop
+  return FBResponseWithStatus(FBCommandStatusNoError, netconnType);
+}
+
++ (id<FBResponsePayload>)handleGetNetBrand:(FBRouteRequest *)request
+{
+  CTTelephonyNetworkInfo *info = [[CTTelephonyNetworkInfo alloc] init];
+  CTCarrier *carinfo = info.subscriberCellularProvider;
+  return FBResponseWithStatus(FBCommandStatusNoError, @{
+                                                        @"name": carinfo.carrierName?:@"",
+                                                        @"MNC": carinfo.mobileNetworkCode?:@"",
+                                                        @"isoCountryCode": carinfo.isoCountryCode?:@"",
+                                                        @"mobileCountryCode": carinfo.mobileCountryCode?:@"",
+                                                        });
 }
 
 @end

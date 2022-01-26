e197eec47a60ac91ebe565d332f49b243d4c894e
csusheep <liuxiaodong@uusense.com>
2022-01-26 17:34:02 +0800

    1、修复iOS15遗下至iOS14.1以上（包含）无法区域截图的问题；

--- WebDriverAgentLib/Commands/FBScreenshotCommands.m
+++ WebDriverAgentLib/Commands/FBScreenshotCommands.m
@@ -25,6 +25,8 @@
 #import "FBScreenshot.h"
 #import "FBMacros.h"
 #import "FBImageIOScaler.h"
+#import "FBConfiguration.h"
+#import "FBErrorBuilder.h"
 
 static const NSTimeInterval SCREENSHOT_TIMEOUT = 0.5;
 
@@ -40,6 +42,7 @@ static const NSTimeInterval SCREENSHOT_TIMEOUT = 0.5;
     [[FBRoute GET:@"/screenshot"] respondWithTarget:self action:@selector(handleGetScreenshot:)],
     [[FBRoute GET:@"/uusense/screenshot"].withoutSession respondWithTarget:self action:@selector(uu_handleGetScreenshot:)],
     [[FBRoute GET:@"/uusense/screenshot"] respondWithTarget:self action:@selector(uu_handleGetScreenshot:)],
+    [[FBRoute GET:@"/uusense/scalingcreenshot"].withoutSession respondWithTarget:self action:@selector(uu_handleScalingScreenshot:)],
     [[FBRoute POST:@"/uusense/screenshot"].withoutSession respondWithTarget:self action:@selector(uu_handleASScreenshot:)],
     //[[FBRoute POST:@"/uusense/screenshot2"].withoutSession respondWithTarget:self action:@selector(uu_handlePostScreenshot:)],
   ];
@@ -120,6 +123,35 @@ static const NSTimeInterval SCREENSHOT_TIMEOUT = 0.5;
         return UUResponseWithJPG(screenshotData);
       }
     }
+    
+    if (version.doubleValue >= 14.1) {
+      
+        rect = CGRectMake((CGFloat)[request.arguments[@"x"] doubleValue], (CGFloat)[request.arguments[@"y"] doubleValue], (CGFloat)[request.arguments[@"width"] doubleValue], (CGFloat)[request.arguments[@"height"] doubleValue]);
+      
+        if (rect.origin.x < 0 || rect.origin.y < 0 || (0.0 == rect.size.height && 0.0 == rect.size.width) || fullScreen) {
+          rect = CGRectNull;
+        }
+        
+        CGFloat screenshotCompressionQuality = 0.6;
+      
+        //id<XCTestManager_ManagerInterface> proxy = [FBXCTestDaemonsProxy testRunnerProxy];
+        //dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+        [proxy _XCT_requestScreenshotOfScreenWithID:[[XCUIScreen mainScreen] displayID]
+                                             withRect:rect
+                                                  uti:(__bridge id)kUTTypeJPEG
+                                   compressionQuality:screenshotCompressionQuality
+                                            withReply:^(NSData *data, NSError *err) {
+            if (err != nil) {
+                [FBLogger logFmt:@"Error taking screenshot: %@", [error description]];
+            }
+            screenshotData = data;
+            dispatch_semaphore_signal(sem);
+        }];
+        dispatch_semaphore_wait(sem, dispatch_time(DISPATCH_TIME_NOW, (int64_t)(SCREENSHOT_TIMEOUT * NSEC_PER_SEC)));
+        if (nil != screenshotData) {
+          return UUResponseWithJPG(screenshotData);
+        }
+    }
   }
   
   if (![mainScreen respondsToSelector: @selector(screenshotDataForQuality:rect:error:)]) {
@@ -191,6 +223,94 @@ static const NSTimeInterval SCREENSHOT_TIMEOUT = 0.5;
   } else {
     return UUResponseWithJPG(screenshotData);
   }
+}
+
++ (id<FBResponsePayload>)uu_handleScalingScreenshot:(FBRouteRequest *)request
+{
+  CGFloat scalingFactor = [FBConfiguration mjpegScalingFactor] / 100.0f;
+  BOOL usesScaling = fabs(FBMaxScalingFactor - scalingFactor) > DBL_EPSILON;
+  CGFloat compressionQuality = FBConfiguration.mjpegServerScreenshotQuality / 100.0f;
+  // If scaling is applied we perform another JPEG compression after scaling
+  // To get the desired compressionQuality we need to do a lossless compression here
+  CGFloat screenshotCompressionQuality = usesScaling ? FBMaxCompressionQuality : compressionQuality;
+  NSError *error;
+  NSData *screenshotData = [FBScreenshot takeInOriginalResolutionWithScreenID:[XCUIScreen.mainScreen displayID]
+                                                           compressionQuality:screenshotCompressionQuality
+                                                                          uti:(__bridge id)kUTTypeJPEG
+                                                                      timeout:1.
+                                                                        error:&error];
+  if (nil == screenshotData) {
+    [FBLogger logFmt:@"%@", error.description];
+    FBResponseWithUnknownErrorFormat(@"screenshot error: %@",[error description]);
+  }
+
+  if (usesScaling) {
+    screenshotData = [self.class scaledJpegImageWithImage:screenshotData scalingFactor:scalingFactor compressionQuality:compressionQuality error:&error];
+  }
+  NSString *screenshot = [screenshotData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+  return FBResponseWithObject(screenshot);
+}
+
++  (nullable NSData *)scaledJpegImageWithImage:(NSData *)image
+                                scalingFactor:(CGFloat)scalingFactor
+                           compressionQuality:(CGFloat)compressionQuality
+                                        error:(NSError **)error
+{
+  CGImageSourceRef imageData = CGImageSourceCreateWithData((CFDataRef)image, nil);
+  CGSize size = [self.class imageSizeWithImage:imageData];
+  CGFloat scaledMaxPixelSize = MAX(size.width, size.height) * scalingFactor;
+  CFDictionaryRef params = (__bridge CFDictionaryRef)@{
+    (const NSString *)kCGImageSourceCreateThumbnailWithTransform: @(YES),
+    (const NSString *)kCGImageSourceCreateThumbnailFromImageIfAbsent: @(YES),
+    (const NSString *)kCGImageSourceThumbnailMaxPixelSize: @(scaledMaxPixelSize)
+  };
+  CGImageRef scaled = CGImageSourceCreateThumbnailAtIndex(imageData, 0, params);
+  CFRelease(imageData);
+  if (nil == scaled) {
+    [[[FBErrorBuilder builder]
+      withDescriptionFormat:@"Failed to scale the image"]
+     buildError:error];
+    return nil;
+  }
+  NSData *resData = [self.class jpegDataWithImage:scaled
+                         compressionQuality:compressionQuality];
+  if (nil == resData) {
+    [[[FBErrorBuilder builder]
+      withDescriptionFormat:@"Failed to compress the image to JPEG format"]
+     buildError:error];
+  }
+  CGImageRelease(scaled);
+  return resData;
+}
+
++ (CGSize)imageSizeWithImage:(CGImageSourceRef)imageSource
+{
+  NSDictionary *options = @{
+    (const NSString *)kCGImageSourceShouldCache: @(NO)
+  };
+  CFDictionaryRef properties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, (CFDictionaryRef)options);
+  NSNumber *width = [(__bridge NSDictionary *)properties objectForKey:(const NSString *)kCGImagePropertyPixelWidth];
+  NSNumber *height = [(__bridge NSDictionary *)properties objectForKey:(const NSString *)kCGImagePropertyPixelHeight];
+  CGSize size = CGSizeMake([width floatValue], [height floatValue]);
+  CFRelease(properties);
+  return size;
+}
+
++ (nullable NSData *)jpegDataWithImage:(CGImageRef)imageRef
+                    compressionQuality:(CGFloat)compressionQuality
+{
+  NSMutableData *newImageData = [NSMutableData data];
+  CGImageDestinationRef imageDestination = CGImageDestinationCreateWithData((CFMutableDataRef)newImageData, kUTTypeJPEG, 1, NULL);
+  CFDictionaryRef compressionOptions = (__bridge CFDictionaryRef)@{
+    (const NSString *)kCGImageDestinationLossyCompressionQuality: @(compressionQuality)
+  };
+  CGImageDestinationAddImage(imageDestination, imageRef, compressionOptions);
+  if(!CGImageDestinationFinalize(imageDestination)) {
+    [FBLogger log:@"Failed to write the image"];
+    newImageData = nil;
+  }
+  CFRelease(imageDestination);
+  return newImageData;
 }
 
 
